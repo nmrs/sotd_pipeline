938f9fa657569efc74f199e0ff0d6612c524f71d
Jim Moore <jim@jmoore.org>
2026-01-23 23:17:59 -0600

    test: add brand-diversity to unified monthly generator test
    - Add brand-diversity table to test data
    - All tests now passing

--- tests/report/test_integration.py
+++ tests/report/test_integration.py
@@ -122,6 +122,7 @@ class TestReportIntegration:
             "brushes": [{"name": "Simpson Chubby 2", "shaves": 15, "rank": 1}],
             "soaps": [{"name": "Test Soap", "shaves": 30, "rank": 1}],
             "soap-makers": [{"brand": "Test Brand", "shaves": 30, "rank": 1}],
+            "brand-diversity": [{"brand": "Test Brand", "unique_soaps": 1, "rank": 1}],
             "razor-formats": [{"format": "DE", "shaves": 25, "rank": 1}],
             "razor-manufacturers": [{"brand": "Gillette", "shaves": 25, "rank": 1}],
             "blade-manufacturers": [{"brand": "Gillette", "shaves": 20, "rank": 1}],
